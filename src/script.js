ac7d9c8f98398f1a31485deecc163d8e081aa38a
Marcos Trajano <m.juniorandre18@gmail.com>
2023-05-30 09:03:45 -0300

    first commit

---
+++ src/script.js
@@ -0,0 +1,43 @@
+const controls = document.querySelectorAll(".control");
+let currentItem = 0;
+const items = document.querySelectorAll(".item");
+const maxItems = items.length;
+
+controls.forEach((control) => {
+    control.addEventListener("click", () => {
+        const isLeft = control.classList.contains("arrow-left");
+
+        if(isLeft) {
+            currentItem -= 1;
+        } else {
+            currentItem += 1;
+        }
+
+        if (currentItem >= maxItems) {
+            currentItem = 0;
+        }
+
+        if (currentItem < 0) {
+            currentItem = maxItems - 1;
+        }
+
+        items.forEach((item) => item.classList.remove("current-item"));
+
+        items[currentItem].scrollIntoView({
+            inline: "center",
+            behavior: "smooth",
+            block: "nearest",
+        });
+
+        item[currentItem].classList.add("current-item");
+    });
+});
+
+function menuShow() {
+    let menuMobile = document.querySelector('.menu-mobile');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+    } else {
+        menuMobile.classList.add('open');
+    }
+}
